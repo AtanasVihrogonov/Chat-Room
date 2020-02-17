24e2af2c6c3b39d8be98062d8561183d57d278d3
A.Vihrogonov <vihrogonov@protonmail.com>
2020-02-17 18:18:05 +0000

    initialize the application

--- scripts/app.js
+++ scripts/app.js
@@ -2,10 +2,14 @@
 * Start to initialize the application
 */ 
 
+// DOM queries
+const chatlist = document.querySelector('.chat-list');
+
 // class instances
+const chatUI = new ChatUI(chatlist);
 const chatroom = new Chatroom('general', 'shaun');
 
 // get chat and render
 chatroom.getChats(data => {
-  console.log(data);
+  chatUI.render(data)
 });
